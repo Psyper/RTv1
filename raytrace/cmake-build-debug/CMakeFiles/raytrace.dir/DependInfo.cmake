46042f246bad10dfd52ce0a225683dc8b86e5a4a
Psyper <psycostin@gmail.com>
2021-02-26 11:27:18 +0300

    Add files via upload

---
+++ raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/psy/CLionProjects/raytrace/camera_rotation.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/camera_rotation.c.o"
+  "/home/psy/CLionProjects/raytrace/cone.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/cone.c.o"
+  "/home/psy/CLionProjects/raytrace/cylinder.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/cylinder.c.o"
+  "/home/psy/CLionProjects/raytrace/draw.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/draw.c.o"
+  "/home/psy/CLionProjects/raytrace/ex.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/ex.c.o"
+  "/home/psy/CLionProjects/raytrace/intersects.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/intersects.c.o"
+  "/home/psy/CLionProjects/raytrace/keys_linux.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/keys_linux.c.o"
+  "/home/psy/CLionProjects/raytrace/lft/ft_clamp.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/lft/ft_clamp.c.o"
+  "/home/psy/CLionProjects/raytrace/lft/ft_max.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/lft/ft_max.c.o"
+  "/home/psy/CLionProjects/raytrace/lft/ft_min.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/lft/ft_min.c.o"
+  "/home/psy/CLionProjects/raytrace/main.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/main.c.o"
+  "/home/psy/CLionProjects/raytrace/parse.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/parse.c.o"
+  "/home/psy/CLionProjects/raytrace/parse_ex.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/parse_ex.c.o"
+  "/home/psy/CLionProjects/raytrace/parse_input.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/parse_input.c.o"
+  "/home/psy/CLionProjects/raytrace/plane.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/plane.c.o"
+  "/home/psy/CLionProjects/raytrace/sphere.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/sphere.c.o"
+  "/home/psy/CLionProjects/raytrace/vector_math.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/vector_math.c.o"
+  "/home/psy/CLionProjects/raytrace/vector_math_dop.c" "/home/psy/CLionProjects/raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/vector_math_dop.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ raytrace/cmake-build-debug/CMakeFiles/raytrace.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytrace.dir/camera_rotation.c.o"
+  "CMakeFiles/raytrace.dir/cone.c.o"
+  "CMakeFiles/raytrace.dir/cylinder.c.o"
+  "CMakeFiles/raytrace.dir/draw.c.o"
+  "CMakeFiles/raytrace.dir/ex.c.o"
+  "CMakeFiles/raytrace.dir/intersects.c.o"
+  "CMakeFiles/raytrace.dir/keys_linux.c.o"
+  "CMakeFiles/raytrace.dir/lft/ft_clamp.c.o"
+  "CMakeFiles/raytrace.dir/lft/ft_max.c.o"
+  "CMakeFiles/raytrace.dir/lft/ft_min.c.o"
+  "CMakeFiles/raytrace.dir/main.c.o"
+  "CMakeFiles/raytrace.dir/parse.c.o"
+  "CMakeFiles/raytrace.dir/parse_ex.c.o"
+  "CMakeFiles/raytrace.dir/parse_input.c.o"
+  "CMakeFiles/raytrace.dir/plane.c.o"
+  "CMakeFiles/raytrace.dir/sphere.c.o"
+  "CMakeFiles/raytrace.dir/vector_math.c.o"
+  "CMakeFiles/raytrace.dir/vector_math_dop.c.o"
+  "raytrace"
+  "raytrace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/raytrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
